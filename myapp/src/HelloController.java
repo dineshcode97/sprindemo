3b766d5c245e8c6f94f6ef84f0970a007571b5a2
Dinesh <64348165+dineshcode97@users.noreply.github.com>
2024-04-09 14:52:26 +0530

    Create HelloController.java

---
+++ myapp/src/HelloController.java
@@ -0,0 +1,13 @@
+package myapp.src;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World\n";
+    }
+    
+}
